7a3cf9c4631b5ba42c2264b1be9aaa688826400b
sdfsgsfgdfg <sfgsdfgsfgsfg>
2015-10-13 17:43:04 +0200

    Lösung der Aufgabestellung angepasst.

--- src/exercise2.java
+++ src/exercise2.java
@@ -2,13 +2,19 @@
  * Created by Markus M. on 12.10.15.
  */
 public class exercise2 {
-    public static void main(String[] args) {
-        String str = "Das ist ein Test";
+    static int counter;
 
-        System.out.print("ShiftRight(" + str + ") --> ");
-        System.out.println(ShiftRight(str));
+    public static void main(String[] args) {
+        counter = 0;
+        String str = "HangLoose";
+
+        System.out.print("shiftRight(" + str + ") --> ");
+        System.out.println(shiftRight(str));
     }
-    private static String ShiftRight(String str) {
-        return str.length() <= 0 ? str : str.charAt(str.length() - 1) + str.substring(0, str.length() - 1);
+
+
+    private static String shiftRight(String str) {
+        counter++;
+        return counter == str.length() + 1 ? str : shiftRight(str.substring(str.length() - 1)) + str.substring(0,str.length() - 1);
     }
 }
