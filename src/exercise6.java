d49277e91dbff855709c37e305677d73ee56c31d
sdfsgsfgdfg <sfgsdfgsfgsfg>
2015-10-13 18:17:59 +0200

    Lösung der Aufgabestellung angepasst, keine Fehler behoben.

--- src/exercise6.java
+++ src/exercise6.java
@@ -2,20 +2,24 @@
  * Created by Markus M. on 12.10.15.
  */
 public class exercise6 {
+    static int counter;
+
     public static void main(String[] args) {
+        counter=0;
         String a = "Hang";
         String b = "Loose";
 
-        System.out.println("ShiftRight(" + b + ") --> " + ShiftRight(b));
-        System.out.print("ZipShift(" + a + ", "+ ShiftRight(b) + ") --> ");
-        System.out.println(ZipShift(a, ShiftRight(b))); //Ich hoffe die Aufgabe war so gemeint, sonst kann ich sie nochmal machen :)
+        System.out.println("ShiftRight(" + b + ") --> " + shiftRight(b));
+        System.out.print("ZipShift(" + a + ", "+ shiftRight(b) + ") --> ");
+        System.out.println(ZipShift(a, shiftRight(b))); //Ich hoffe die Aufgabe war so gemeint, sonst kann ich sie nochmal machen :)
     }
     private static String ZipShift(String a, String b) {
         return a.length() + b.length() <= 2 ? a+b :
                 "" + a.charAt(0) + b.charAt(0) + ZipShift(a.substring(1), b.substring(1));
     }
 
-    private static String ShiftRight(String str) {
-        return str.length() <= 0 ? str : str.charAt(str.length() - 1) + str.substring(0, str.length() - 1);
+    private static String shiftRight(String str) {
+        counter++;
+        return counter == str.length() + 1 ? str : shiftRight(str.substring(str.length() - 1)) + str.substring(0,str.length() - 1);
     }
 }
